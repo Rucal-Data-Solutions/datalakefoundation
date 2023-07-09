c699ef04098f22ad1278e55c46f7d739258ffff6
Ruben <114101564+Gateway1024@users.noreply.github.com>
2023-07-09 14:37:10 +0200

    Update datalake.scala

--- src/test/scala/datalake.scala
+++ src/test/scala/datalake.scala
@@ -26,7 +26,7 @@ object DatalakeApp {
   def main(args: Array[String]): Unit = {
 
     val metadatasettings = new SqlMetadataSettings()
-    metadatasettings.initialize(new SqlServerSettings("2ssdataplatformserver.database.windows.net", 1433, "2SSTB-DATAPLATFORM-SQL-MGMT", "databricks_acc", "#T388K8$@k7r"))
+    metadatasettings.initialize(new SqlServerSettings("2ssdataplatformserver.database.windows.net", 1433, "2SSTB-DATAPLATFORM-SQL-MGMT", "<USER>", "<PASSWORD>"))
 
     val metadata = new Metadata(metadatasettings)
 
