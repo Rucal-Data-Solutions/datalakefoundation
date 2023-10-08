ee7da84f792475229934bb30325c426958e0f990
Ruben <ruben@rucal.nl>
2023-10-08 23:32:57 +0200

    corrected a typo

--- src/main/scala/datalake/metadata/datafactory.scala
+++ src/main/scala/datalake/metadata/datafactory.scala
@@ -2,25 +2,25 @@ package datalake.metadata
 
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
+import org.json4s.FieldSerializer.{ignore}
 import org.json4s.jackson.Serialization
 import org.json4s.jackson.Serialization.{ read, write }
-import org.json4s.FieldSerializer.{ renameTo, ignore, renameFrom }
 
 object DataFactory {
 
-  def watermarkFunction: PartialFunction[(String, Any), Option[(String, Any)]] = { 
+  def watermarkFunction: PartialFunction[(String, Any), Option[(String, Any)]] = {
     case ("environment", _) => None
-    case ("function", f) => Some(("fun", f))
+    case ("function", f)    => Some(("function", f))
   }
 
-
   def getConfigItems(connection: Connection): String = {
-    implicit val formats: Formats = DefaultFormats + FieldSerializer[Entity](ignore("metadata") orElse ignore("environment")) + FieldSerializer[EntityColumn]() + FieldSerializer[Watermark](watermarkFunction)
+    implicit val formats: Formats = DefaultFormats + FieldSerializer[Entity](
+      ignore("metadata") orElse ignore("environment")
+    ) + FieldSerializer[EntityColumn]() + FieldSerializer[Watermark](watermarkFunction)
 
     // val _settings = write(connection.getSettings)
-    val _entities = write(connection.getEntities.filter(p => p.isEnabled() == true))
-
-    _entities
+    val enabled_entities = connection.getEntities.filter(p => p.isEnabled() == true)
+    write(enabled_entities)
 
   }
 
