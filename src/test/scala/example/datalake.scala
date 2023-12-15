bb11399c0f95dd601e4afc49a3357c3ede788f9a
Ruben <ruben@rucal.nl>
2023-12-15 15:36:52 +0100

    Refactor DatalakeApp to DatalakeTestApp and update metadata.json

--- src/test/scala/example/datalake.scala
+++ src/test/scala/example/datalake.scala
@@ -5,12 +5,11 @@ import org.apache.spark.sql.{ SparkSession, DataFrame }
 
 import datalake.metadata._
 import datalake.processing._
-import org.apache.parquet.format.IntType
 
 // import org.apache.spark.sql._
 // import org.apache.spark.sql.functions.{col}
 
-object DatalakeApp {
+object DatalakeTestApp {
 
   val spark = SparkSession
     .builder()
@@ -25,50 +24,22 @@ object DatalakeApp {
 
   def main(args: Array[String]): Unit = {
 
-    val metadatasettings = new SqlMetadataSettings()
+    val metadatasettings = new JsonMetadataSettings()
+
+    val user_dir = System.getProperty("user.dir")
+    println(user_dir)
+
+    metadatasettings.initialize(f"${user_dir}\\src\\test\\scala\\example\\metadata.json")
 
     implicit val metadata = new Metadata(metadatasettings)
+    println(metadata.getEnvironment.Name)
 
-    val connection = metadata.getConnection("1")
-    val dfItems = DataFactory.getConfigItems(connection)
+    val connection = metadata.getConnectionByName("sql_test")
 
-    val entity = metadata.getEntity(39)
+    val adf_config = DataFactory.getConfigItems(connection)
 
-    val proc = new Processing(entity, "test.parquet")
-    
-    proc.process()
+    println(adf_config)
+  
  }
 
-  def getTestParquet(): DataFrame = {
-    import org.apache.spark.sql.Row
-    import org.apache.spark.sql.types._
-    import spark.implicits._
-
-// Define the schema
-    val schema = StructType(
-      Seq(
-        StructField("id", IntegerType, true),
-        StructField("name", StringType, true),
-        StructField("dec_value", DecimalType(38, 1), true),
-        StructField("description", StringType, true),
-        StructField("new_field", BooleanType, true),
-      )
-    )
-
-// Create a sequence of rows
-    val data = Seq(
-      Row(1, "Alice", Decimal(100.5), "Desc1", false),
-      Row(2, "Bob", Decimal(200.5), "Desc2", false),
-      Row(3, "Charlie", Decimal(300.5), "Test_123", false),
-      Row(4, "Gerald", Decimal(300.5), "Test_345", false),
-      Row(5, "Ruben", Decimal(10000.45), "asdsfdgs", true)
-    )
-
-// Create a DataFrame with the given schema and data
-    val df = spark.createDataFrame(
-      spark.sparkContext.parallelize(data),
-      schema
-    )
-    df
-  }
 }
