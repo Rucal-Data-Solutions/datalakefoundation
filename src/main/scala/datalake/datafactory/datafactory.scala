d04ddbb605ee2b5ed543a9cab860457c1b83f06e
Ruben <ruben@rucal.nl>
2023-12-30 21:39:36 +0100

    Better error message on invalid argument type

--- src/main/scala/datalake/datafactory/datafactory.scala
+++ src/main/scala/datalake/datafactory/datafactory.scala
@@ -5,7 +5,6 @@ import org.json4s.jackson.JsonMethods._
 import org.json4s.FieldSerializer.{ignore}
 import org.json4s.jackson.Serialization
 import org.json4s.jackson.Serialization.{ read, write }
-import scala.reflect.ClassTag
 
 import datalake.core._
 import datalake.metadata._
@@ -17,19 +16,19 @@ object DataFactory {
    * Retrieves the configuration items for the given object.
    *
    * @param obj The object for which configuration items are to be retrieved.
-   *            Accepted data types: EntityGroup, Connection.
+   *            Accepted data types: EntityGroup, Connection, EntityId(INT)
    * @param metadata The metadata containing information about the entities and connections.
    * @return A string representation of the enabled entities for the given object.
    * @throws IllegalArgumentException if the object type is invalid.
    */
-  def getConfigItems(obj: Any)(implicit metadata: Metadata): String = {
+  def getConfigItems(arg: Any)(implicit metadata: Metadata): String = {
     implicit val formats: Formats = DefaultFormats + FieldSerializer[EntityColumn]() + new EntitySerializer(metadata) + new WatermarkSerializer(metadata)
 
-    val entities = obj match {
+    val entities = arg match {
       case group: EntityGroup => metadata.getEntities(group)
       case connection: Connection => metadata.getEntities(connection)
       case entityId: Int => metadata.getEntities(entityId)
-      case _ => throw new IllegalArgumentException("Invalid type.")
+      case _ => throw new IllegalArgumentException(s"Invalid parameter type ${arg.getClass().getTypeName()}")
     }
 
     val enabledEntities = entities.filter(_.isEnabled)
