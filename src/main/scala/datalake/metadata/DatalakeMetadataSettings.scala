f025ff90677d3ffeda6a33390dc807a98a359f5e
Ruben <ruben@rucal.nl>
2023-12-30 20:39:06 +0100

    removed unused references

--- src/main/scala/datalake/metadata/DatalakeMetadataSettings.scala
+++ src/main/scala/datalake/metadata/DatalakeMetadataSettings.scala
@@ -2,17 +2,7 @@ package datalake.metadata
 
 import datalake.core._
 import datalake.processing._
-import java.util.TimeZone
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{ DataFrame, Column, Row, Dataset }
-import org.apache.spark.sql.types._
 import scala.util.Try
-import scala.reflect.runtime._
-import org.json4s.JsonAST
-import scala.tools.cmd.Meta
-import org.apache.arrow.flatbuf.Bool
-import datalake.core.EntityGroup
 
 trait DatalakeMetadataSettings {
   type initParam
