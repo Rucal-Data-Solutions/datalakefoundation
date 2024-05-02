cb11f6d267ccc13283ce397a52fe552bb53eaba9
Ruben <ruben@rucal.nl>
2024-05-02 17:40:39 +0200

    added partitionOverwriteMode dynamic to Full load

--- src/main/scala/datalake/processing/Full.scala
+++ src/main/scala/datalake/processing/Full.scala
@@ -27,9 +27,19 @@ final object Full extends ProcessStrategy {
     val datalake_source = processing.getSource
     val source: DataFrame = datalake_source.source
 
-    val part_values: List[String] = datalake_source.partition_columns.getOrElse(List.empty).map(_._1)
+    val part_values: List[String] =
+      datalake_source.partition_columns.getOrElse(List.empty).map(_._1)
 
-    source.write.partitionBy(part_values:_*).mode(SaveMode.Overwrite).option("overwriteSchema", "True").delta(processing.destination)
+    source.write
+      .partitionBy(part_values: _*)
+      .mode(SaveMode.Overwrite)
+      .options(
+        Map[String, String](
+          ("overwriteSchema", "True"),
+          ("partitionOverwriteMode", "dynamic")
+        )
+      )
+      .delta(processing.destination)
 
   }
 }
