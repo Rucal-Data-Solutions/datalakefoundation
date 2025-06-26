9e39932a5c63c39e944cde4d492dac79679e7b53
Ruben <gateway1024@gmail.com>
2025-06-26 10:52:17 +0200

    Optimize getLastValue method in WatermarkData to retrieve the latest timestamp using aggregation

--- src/main/scala/datalake/core/WatermarkData.scala
+++ src/main/scala/datalake/core/WatermarkData.scala
@@ -30,7 +30,7 @@ final class WatermarkData(entity_id: Integer)(implicit environment: Environment)
       try {
         val lastRow = df.get
           .filter((col("entity_id") === entity_id) && (col("column_name") === column_name))
-          .sort(col("timestamp").desc)
+          .agg(max("timestamp"))
           .head()
         val _value = lastRow.getAs[String]("value")
         val _datatype = lastRow.getAs[String]("source_type")
