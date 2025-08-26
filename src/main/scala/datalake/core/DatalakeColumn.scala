7d612fa16457b42d719abe4fe50004797727fd2b
Ruben <114101564+Gateway1024@users.noreply.github.com>
2025-08-26 12:30:05 +0200

    refactor: restore custom equals and toString

--- src/main/scala/datalake/core/DatalakeColumn.scala
+++ src/main/scala/datalake/core/DatalakeColumn.scala
@@ -1,40 +1,22 @@
 package datalake.core
 
-import datalake.metadata._
-import org.apache.spark.sql.{ DataFrame, SparkSession, Row }
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.functions.col
-import io.delta.tables._
-import java.sql.Timestamp
-import com.fasterxml.jackson.module.scala.deser.overrides
+import org.apache.spark.sql.types.DataType
 
-class DatalakeColumn(
-    val name: String,
-    val dataType: DataType,
-    val nullable: Boolean,
-    val partOfPartition: Boolean
-) extends Serializable {
+case class DatalakeColumn(
+    name: String,
+    dataType: DataType,
+    nullable: Boolean,
+    partOfPartition: Boolean = false
+) {
 
-  override def toString(): String = this.name
+    override def toString: String = this.name
 
-  override def equals(obj: Any): Boolean = {
-    obj match {
-      case col: DatalakeColumn =>
-        this.name == col.name
-      case _ => false
+    override def equals(obj: Any): Boolean = {
+        obj match {
+            case col: DatalakeColumn => this.name == col.name
+            case _ => false
+        }
     }
-  }
-}
 
-object DatalakeColumn {
-
-  def apply(
-      name: String,
-      data_type: DataType,
-      nullable: Boolean,
-      part_of_partition: Boolean
-  ): DatalakeColumn =
-    new DatalakeColumn(name, data_type, nullable, part_of_partition)
-
-  def apply(name: String, data_type: DataType,nullable: Boolean): DatalakeColumn = new DatalakeColumn(name, data_type, nullable, false)
+    override def hashCode(): Int = name.hashCode
 }
